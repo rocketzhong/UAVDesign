6f1ae95a8f018da36f5c23c40075f64cdbf99e69
kumlowhup <kumlowhup@yahoo.co.jp>
2023-02-22 21:59:34 +0800

    3dmodel import

---
+++ frontend/src/model3d.vue
@@ -0,0 +1,49 @@
+<template>
+    <canvas id="uav-model" width="400" height="400"></canvas>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import * as THREE from 'three';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+onMounted(() => {
+    const canvas = document.querySelector('#uav-model')
+    const scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xbbbbbb)
+    // 创建渲染器
+    const renderer = new THREE.WebGLRenderer({
+        canvas,
+        antialias: true
+    });
+    const { width, height } = canvas;
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    // 相机
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000);
+    camera.position.set(-7, 3, 0);
+    camera.lookAt(0, -1, 0);
+
+    // 光线
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+    hemiLight.position.set(0, 20, 0);
+    scene.add(hemiLight);
+    // 加载模型
+    const loader = new GLTFLoader().setPath('assets/');
+    loader.load('uav.glb', (gltf) => {
+        scene.add(gltf.scene);
+    });
+    const animationFrame = () => {
+        renderer.render(scene, camera);
+        window.requestAnimationFrame(animationFrame);
+    };
+    window.requestAnimationFrame(animationFrame);
+
+})
+</script>
+
+<style lang="scss" scoped></style>
